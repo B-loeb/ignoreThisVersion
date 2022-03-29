79f06d3f5a2d241b817449d063907b497cbd640c
B-loeb <bradleyloeb@gmail.com>
2022-03-28 21:09:16 -0400

    add img to error page

--- views/error404.jsx
+++ views/error404.jsx
@@ -9,7 +9,8 @@ function error404 () {
               <p>Oops, sorry, we can't find this page!</p>
               <div>
                 <img src="/images/errorCream.jpg" alt="Dropped Ice Cream Cone"/>
-               <p> Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/s/photos/error?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+               <p> 
+               Photo by <a href="https://unsplash.com/photos/52jRtc2S_VE">Sarah Kilian</a> on <a href="https://unsplash.com/">Unsplash</a>
                </p>
               </div>
           </main>
