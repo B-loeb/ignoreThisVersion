0d34b2432f8703df4fc8e07cf07c2b225fc4fe40
B-loeb <bradleyloeb@gmail.com>
2022-03-30 21:10:18 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const places = require('../models/places.js')
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -18,8 +18,18 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      //default img if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'  
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'Usa'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 router.get('/new', (req, res) => {
