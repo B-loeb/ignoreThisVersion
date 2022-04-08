13069c665e2a77410a97c01f68534b0ecc209daf
B-loeb <bradleyloeb@gmail.com>
2022-04-08 15:59:00 -0400

    debugging

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -12,7 +12,8 @@ function show (data) {
                  Not yet Rated!
              </h3>
          )
-      return <Def>
+      (
+      <Def>
           <main>
             <h1>{data.place.name}</h1>
             <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
@@ -25,7 +26,48 @@ function show (data) {
         </form>     
      </main>
  </Def>
-}
+  )
 
+  return (
+    <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{ data.place.name }</h1>
+            <h2>
+              Rating
+            </h2>
+            {rating}
+            <br />
+            <h2>
+              Description
+            </h2>
+            <h3>
+              {data.place.showEstablished()}
+            </h3>
+            <h4>
+              Serving {data.place.cuisines}
+            </h4>
+            <br />
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>{` `}
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+        </main>
+    </Def>
+)
+}
 
 module.exports = show
